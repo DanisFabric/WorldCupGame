70a31c20091fa02e6245e8f9645ebfefdbe0e433
liaolei <liaolei@gotokeep.com>
2018-05-16 15:06:11 +0800

    测试方法调用

---
+++ test.js
@@ -0,0 +1,30 @@
+const Nebulas = require("nebulas");
+
+const neb = new Nebulas.Neb();
+
+neb.setRequest(new Nebulas.HttpRequest("https://testnet.nebulas.io"));
+
+const address = "n1NEYDpueKsMuy6M3wCThTXwCr6NuCqGS7n";
+const contractAddress = "n1pwyHjaZe1UEJVh8LzagbpuqJESWv6BRgp";
+
+neb.api.getAccountState(address).then(function (state) {
+    console.log(state);
+    neb.api.call({
+        chainID: 1001,
+        from: address,
+        to: contractAddress,
+        value: 0,
+        nonce: parseInt(state.nonce) + 1,
+        gasPrice: 1000000,
+        gasLimit: 2000000,
+        contract: {
+            function: "getCountries",
+            args: ""
+        }
+    }).then(function (x) {
+       console.log(x);
+    }).catch(function (err) {
+        console.log(err);
+    });
+});
+
