ed741a1913e9c0bc492896f9781bfde96f143748
liaolei <liaolei@gotokeep.com>
2018-05-17 16:36:06 +0800

    完成除设定冠军以外的所有接口测试

--- test.js
+++ test.js
@@ -5,8 +5,15 @@ const neb = new Nebulas.Neb();
 
 neb.setRequest(new Nebulas.HttpRequest("https://testnet.nebulas.io"));
 
-const address = "n1b18cYuzp2bS14KPwC7cyF38Pe4JHaJKdy";
-const contractAddress = "n1zQkQPVYW9UGb7YRxtdjkW4NcZn26QhhW7";
+const address = "";
+const contractAddress = "n1zzniVioVyqb1cRud8frqM3f1nVo7pPc4Q";
+
+let testAccounts = [
+    {address: "n1b18cYuzp2bS14KPwC7cyF38Pe4JHaJKdy", privateKey: "608c2daab9859ae9793aadd2432236df4587803d2b2cf6f37415751ea6b72b1d"},
+    {address: "", privateKey: ""},
+    {address: "", privateKey: ""},
+    {address: "", privateKey: ""}
+];
 
 function nasToWei(value) {
     return toBigNumber(value).times(toBigNumber(10).pow(18));
@@ -15,13 +22,12 @@ function toBigNumber(value) {
     return new BigNumber(value);
 }
 
-
-function readCountries() {
-    neb.api.getAccountState(address).then(function (state) {
+function getCountries() {
+    neb.api.getAccountState(testAccounts[0].address).then(function (state) {
         console.log(state);
         neb.api.call({
             chainID: 1001,
-            from: address,
+            from: testAccounts[0].address,
             to: contractAddress,
             value: 1,
             nonce: parseInt(state.nonce) + 1,
@@ -40,10 +46,10 @@ function readCountries() {
 }
 
 function addLottery() {
-    neb.api.getAccountState(address).then (function (state) {
+    neb.api.getAccountState(testAccounts[0].address).then (function (state) {
        console.log(state);
-       const account = Nebulas.Account.fromAddress(address);
-       account.setPrivateKey("608c2daab9859ae9793aadd2432236df4587803d2b2cf6f37415751ea6b72b1d");
+       const account = Nebulas.Account.fromAddress(testAccounts[0].address);
+       account.setPrivateKey(testAccounts[0].privateKey);
 
         const tx = new Nebulas.Transaction({
             chainID: 1001,
@@ -74,21 +80,79 @@ function addLottery() {
 }
 
 function getAllLotteries() {
-    
+    neb.api.getAccountState(testAccounts[0].address).then(function (state) {
+        console.log(state);
+        neb.api.call({
+            chainID: 1001,
+            from: testAccounts[0].address,
+            to: contractAddress,
+            value: 0,
+            nonce: parseInt(state.nonce) + 1,
+            gasPrice: 1000000,
+            gasLimit: 2000000,
+            contract: {
+                function: "getAllLotteries",
+                args: ""
+            }
+        }).then(function (x) {
+            console.log(x);
+        }).catch(function (err) {
+            console.log(err);
+        });
+    });
 }
 
-// function unlockAccount() {
-//     neb.admin.unlockAccount({
-//         address: address,
-//         passphrase: "danis12315",
-//         duration: 10000000000
-//     }).then (function (isUnlocked) {
-//         console.log(isUnlocked);
-//     }).catch(function (err) {
-//         console.log(err);
-//     });
-// }
+function getCountryBalanceMap() {
+    neb.api.getAccountState(testAccounts[0].address).then(function (state) {
+        console.log(state);
+        neb.api.call({
+            chainID: 1001,
+            from: testAccounts[0].address,
+            to: contractAddress,
+            value: 0,
+            nonce: parseInt(state.nonce) + 1,
+            gasPrice: 1000000,
+            gasLimit: 2000000,
+            contract: {
+                function: "getCountryBalanceMap",
+                args: ""
+            }
+        }).then(function (x) {
+            console.log(x);
+        }).catch(function (err) {
+            console.log(err);
+        });
+    });
+}
 
-// readCountries();
-// unlockAccount();
+function getBalance() {
+    neb.api.getAccountState(testAccounts[0].address).then(function (state) {
+        console.log(state);
+        neb.api.call({
+            chainID: 1001,
+            from: testAccounts[0].address,
+            to: contractAddress,
+            value: 0,
+            nonce: parseInt(state.nonce) + 1,
+            gasPrice: 1000000,
+            gasLimit: 2000000,
+            contract: {
+                function: "getBalance",
+                args: ""
+            }
+        }).then(function (x) {
+            console.log(x);
+        }).catch(function (err) {
+            console.log(err);
+        });
+    });
+}
+
+// 获取国家列表
+// getCountries();
+// 提交竞猜信息
 // addLottery();
+// 获取所有竞猜列表
+// getAllLotteries();
+// 获得当前合约总竞猜金额
+// getBalance();
